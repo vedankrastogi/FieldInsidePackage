92e25c8b073a469d9a020873793208b80034094c
vedankrastogi <79106599+vedankrastogi@users.noreply.github.com>
2021-05-11 22:39:46 +0530

    PrivateFieldsInsidePackageWithGetterAndSetter

---
+++ src/Package/Implementation.java
@@ -0,0 +1,54 @@
+package Package;
+
+public class Implementation {
+    private int accountNumber;
+    private int balance;
+    private String customerName;
+    private String email;
+    private Long phoneNumber;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int depositfund(int amount){
+        this.balance=getBalance()+amount;
+        return balance;
+    }
+    
+}
